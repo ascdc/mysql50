a9d15c9cf4efa2c73a2f29d36c8a7f66e004800b
chen21019 <chen21019@gate.sinica.edu.tw>
2017-09-19 10:19:39 +0800

    first commit

--- Dockerfile
+++ Dockerfile
@@ -9,9 +9,15 @@ RUN chmod 755 /run.sh && \
 RUN cd /root && \
 	wget https://downloads.mysql.com/archives/get/file/MySQL-server-5.0.96-1.glibc23.x86_64.rpm && \
 	wget https://downloads.mysql.com/archives/get/file/MySQL-shared-compat-5.0.96-1.glibc23.x86_64.rpm && \
+	wget https://downloads.mysql.com/archives/get/file/MySQL-client-5.0.96-1.glibc23.x86_64.rpm && \
 	rpm -Uvh MySQL-shared-compat-5.0.96-1.glibc23.x86_64.rpm && \
 	rpm -Uvh MySQL-server-5.0.96-1.glibc23.x86_64.rpm && \
+	rpm -Uvh MySQL-client-5.0.96-1.glibc23.x86_64.rpm && \
 	rm -f /root/*.rpm
+
+ENV MYSQL_USER **None**
+ENV MYSQL_PASSWORD **None**
+ENV MYSQL_DATABASE **None**
 
 EXPOSE 3306
 WORKDIR /
--- run.sh
+++ run.sh
@@ -2,4 +2,51 @@
 
 /etc/init.d/mysql start
 
+if [ ! -f /.mysql_set ]; then
+	
+	PASS=${MYSQL_ROOT_PASSWORD:-$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-16};echo;)}
+	_word=$( [ ${MYSQL_ROOT_PASSWORD} ] && echo "preset" || echo "random" )
+	echo "=> Setting a ${_word} password to the mysql root user"
+	
+	echo "DROP DATABASE test;"|mysql -sN
+	echo "update mysql.user set host='%',password=PASSWORD('${PASS}') where host='127.0.0.1';"|mysql -sN
+	echo "delete from mysql.user where host!='%';"|mysql -sN
+	
+	if [ "${MYSQL_USER}" != "**None**" ] && [ "${MYSQL_PASSWORD}" != "**None**" ]; then
+		echo "CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"|mysql -sN
+		echo "=> Create a User."
+		echo "User: ${MYSQL_USER}"
+		echo "Password: ${MYSQL_PASSWORD}"
+	fi
+	
+	if [ "${MYSQL_DATABASE}" != "**None**" ]; then
+		IFS=',' read -r -a array <<< "${MYSQL_DATABASE}"
+		
+		for element in "${array[@]}"
+		do
+			echo "CREATE DATABASE $element;"|mysql -sN
+			echo "=> Create a Database $element."
+			
+			if [ "${MYSQL_USER}" != "**None**" ] && [ "${MYSQL_PASSWORD}" != "**None**" ]; then
+				echo "GRANT ALL PRIVILEGES ON $element.* TO '${MYSQL_USER}'@'%';"|mysql -sN
+				echo "=> Grant Database $element All Privileges for ${MYSQL_USER}."
+			fi
+			
+		done
+	fi
+	
+	echo "FLUSH PRIVILEGES;"|mysql -sN
+	
+	touch /.mysql_set
+	
+	echo "========================================================================"
+	echo "You can now connect to this MySQL 5.0.96 container"
+	echo "and enter the root password '$PASS' when prompted"
+	echo ""
+	echo "Please remember to change the above password as soon as possible!"
+	echo "========================================================================"
+fi
+
 /bin/bash
+
+
